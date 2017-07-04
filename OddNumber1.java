f132b1e824aab26e4022e8d7d3871df425e0f1de
Karthikaveni <karthikaveni18@gmail.com>
2017-07-04 16:54:56 +0530

    Create  OddNumber1.java

---
+++ OddNumber1.java
@@ -0,0 +1,21 @@
+# pp
+import java.util.*;
+import java.io.*;
+public class OddNumber1
+{
+public static void main(String args [])
+{
+Scanner sc = new Scanner(System.in);
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+  
+  for(int i=n1;i<=n2;i++)
+  {
+    
+    if(i % 2 == 1)
+    {
+    System.out.print(i + " ");
+    }
+}
+   }
+   }
