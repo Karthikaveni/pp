05a5fa6b46045815c7b08fbc41aac795b16c5ac7
Karthikaveni <karthikaveni18@gmail.com>
2017-07-04 16:43:16 +0530

    Update and rename prime.java to PrimeNumber1.java

---
+++ PrimeNumber1.java
@@ -0,0 +1,25 @@
+# pp
+import java.io.*;
+import java.util.*;
+public class PrimeNumber1{
+public static void main(String args[]){
+ System. out.println("Enter a prime number");
+ 
+   
+   Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+ 
+  int i,m=0,flag=0;    
+   
+  m=n/2;    
+  for(i=2;i<=m;i++){    
+   if(n%i==0){    
+   System.out.println("Number is not prime");    
+   flag=1;    
+   break;    
+   }    
+  }    
+  if(flag==0)    
+  System.out.println("Number is prime");    
+}  
+}  
